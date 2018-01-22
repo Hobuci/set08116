5fe3ef16773a379269d796c713a1f5d79f3f8144
Hobuci <borbely.richard.gm@gmail.com>
2018-01-22 16:43:02 +0000

    17

--- labs/practicals/17_Full_Transformation/17_Full_Transformation.cpp
+++ labs/practicals/17_Full_Transformation/17_Full_Transformation.cpp
@@ -14,90 +14,90 @@ float total_time = 0.0f;
 float theta = 0.0f;
 
 bool load_content() {
-  geom.set_type(GL_TRIANGLE_STRIP);
-  // Create quad data
-  // Positions
-  vector<vec3> positions{vec3(-1.0f, -1.0f, 0.0f), vec3(1.0f, -1.0f, 0.0f), vec3(-1.0f, 1.0f, 0.0f),
-                         vec3(1.0f, 1.0f, 0.0f)};
-  // Colours
-  vector<vec4> colours{vec4(1.0f, 0.0f, 0.0f, 1.0f), vec4(1.0f, 0.0f, 0.0f, 1.0f), vec4(1.0f, 0.0f, 0.0f, 1.0f),
-                       vec4(1.0f, 0.0f, 0.0f, 1.0f)};
-  // Add to the geometry
-  geom.add_buffer(positions, BUFFER_INDEXES::POSITION_BUFFER);
-  geom.add_buffer(colours, BUFFER_INDEXES::COLOUR_BUFFER);
+	geom.set_type(GL_TRIANGLE_STRIP);
+	// Create quad data
+	// Positions
+	vector<vec3> positions{ vec3(-1.0f, -1.0f, 0.0f), vec3(1.0f, -1.0f, 0.0f), vec3(-1.0f, 1.0f, 0.0f),
+						   vec3(1.0f, 1.0f, 0.0f) };
+	// Colours
+	vector<vec4> colours{ vec4(1.0f, 0.0f, 0.0f, 1.0f), vec4(1.0f, 0.0f, 0.0f, 1.0f), vec4(1.0f, 0.0f, 0.0f, 1.0f),
+						 vec4(1.0f, 0.0f, 0.0f, 1.0f) };
+	// Add to the geometry
+	geom.add_buffer(positions, BUFFER_INDEXES::POSITION_BUFFER);
+	geom.add_buffer(colours, BUFFER_INDEXES::COLOUR_BUFFER);
 
-  // Load in shaders
-  eff.add_shader("shaders/basic.vert", GL_VERTEX_SHADER);
-  eff.add_shader("shaders/basic.frag", GL_FRAGMENT_SHADER);
-  // Build effect
-  eff.build();
+	// Load in shaders
+	eff.add_shader("shaders/basic.vert", GL_VERTEX_SHADER);
+	eff.add_shader("shaders/basic.frag", GL_FRAGMENT_SHADER);
+	// Build effect
+	eff.build();
 
-  // Set camera properties
-  cam.set_position(vec3(10.0f, 10.0f, 10.0f));
-  cam.set_target(vec3(0.0f, 0.0f, 0.0f));
-  auto aspect = static_cast<float>(renderer::get_screen_width()) / static_cast<float>(renderer::get_screen_height());
-  cam.set_projection(quarter_pi<float>(), aspect, 2.414f, 1000.0f);
-  return true;
+	// Set camera properties
+	cam.set_position(vec3(10.0f, 10.0f, 10.0f));
+	cam.set_target(vec3(0.0f, 0.0f, 0.0f));
+	auto aspect = static_cast<float>(renderer::get_screen_width()) / static_cast<float>(renderer::get_screen_height());
+	cam.set_projection(quarter_pi<float>(), aspect, 2.414f, 1000.0f);
+	return true;
 }
 
 bool update(float delta_time) {
-  // Accumulate time
-  total_time += delta_time;
-  // Update the scale - base on sin wave
-  s = 1.0f + sinf(total_time);
-  // Multiply by 5
-  s *= 5.0f;
-  // Increment theta - half a rotation per second
-  theta += pi<float>() * delta_time;
-  // Check if key is pressed
-  if (glfwGetKey(renderer::get_window(), GLFW_KEY_UP)) {
-    pos += vec3(0.0f, 0.0f, -5.0f) * delta_time;
-  }
-  if (glfwGetKey(renderer::get_window(), GLFW_KEY_DOWN)) {
-    pos += vec3(0.0f, 0.0f, 5.0f) * delta_time;
-  }
-  if (glfwGetKey(renderer::get_window(), GLFW_KEY_LEFT)) {
-    pos += vec3(-5.0f, 0.0f, 0.0f) * delta_time;
-  }
-  if (glfwGetKey(renderer::get_window(), GLFW_KEY_RIGHT)) {
-    pos += vec3(5.0f, 0.0f, 0.0f) * delta_time;
-  }
-  // Update the camera
-  cam.update(delta_time);
-  return true;
+	// Accumulate time
+	total_time += delta_time;
+	// Update the scale - base on sin wave
+	s = 1.0f + sinf(total_time);
+	// Multiply by 5
+	s *= 5.0f;
+	// Increment theta - half a rotation per second
+	theta += pi<float>() * delta_time;
+	// Check if key is pressed
+	if (glfwGetKey(renderer::get_window(), GLFW_KEY_UP)) {
+		pos += vec3(0.0f, 0.0f, -5.0f) * delta_time;
+	}
+	if (glfwGetKey(renderer::get_window(), GLFW_KEY_DOWN)) {
+		pos += vec3(0.0f, 0.0f, 5.0f) * delta_time;
+	}
+	if (glfwGetKey(renderer::get_window(), GLFW_KEY_LEFT)) {
+		pos += vec3(-5.0f, 0.0f, 0.0f) * delta_time;
+	}
+	if (glfwGetKey(renderer::get_window(), GLFW_KEY_RIGHT)) {
+		pos += vec3(5.0f, 0.0f, 0.0f) * delta_time;
+	}
+	// Update the camera
+	cam.update(delta_time);
+	return true;
 }
 
 bool render() {
-  // Bind effect
-  renderer::bind(eff);
-  mat4 T, R, S, M;
-  // *********************************
-  // Create transformation matrices
-  // ******************************
-
-
-
-  // Combine matrices to set M - remember multiplication order
-
-  // *********************************
-  // Create MVP matrix
-  auto V = cam.get_view();
-  auto P = cam.get_projection();
-  auto MVP = P * V * M;
-  // Set MVP matrix uniform
-  glUniformMatrix4fv(eff.get_uniform_location("MVP"), 1, GL_FALSE, value_ptr(MVP));
-  // Render geometry
-  renderer::render(geom);
-  return true;
+	// Bind effect
+	renderer::bind(eff);
+	mat4 T, R, S, M;
+	// *********************************
+	// Create transformation matrices
+	// ******************************
+	T = translate(mat4(1.f), pos);
+	R = rotate(mat4(1.f), theta, vec3(0.f, 0.f, 1.f));
+	S = scale(mat4(1.f), vec3(s, s, s));
+	// Combine matrices to set M - remember multiplication order
+	M = T * (R * S);
+	// *********************************
+	// Create MVP matrix
+	auto V = cam.get_view();
+	auto P = cam.get_projection();
+	auto MVP = P * V * M;
+	// Set MVP matrix uniform
+	glUniformMatrix4fv(eff.get_uniform_location("MVP"), 1, GL_FALSE, value_ptr(MVP));
+	// Render geometry
+	renderer::render(geom);
+	return true;
 }
 
 void main() {
-  // Create application
-  app application("17_Full_Transformation");
-  // Set load content, update and render methods
-  application.set_load_content(load_content);
-  application.set_update(update);
-  application.set_render(render);
-  // Run application
-  application.run();
+	// Create application
+	app application("17_Full_Transformation");
+	// Set load content, update and render methods
+	application.set_load_content(load_content);
+	application.set_update(update);
+	application.set_render(render);
+	// Run application
+	application.run();
 }
